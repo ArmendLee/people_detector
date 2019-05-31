931516702b6f8abcf80514535d5d84d6b14ce5df
ArmendLee <1003255122@qq.com>
2019-05-31 14:08:34 +0800

    all

---
+++ src/people_detector/src/sub_detect_pub.cpp
@@ -0,0 +1,108 @@
+#include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/core/core.hpp>
+
+
+#include <chrono>
+#include <iostream>
+#include <memory>
+
+
+#include "/home/lhd/Documents/vaml-master/caffe-detectors/include/caffedetector.h"
+#include "/home/lhd/Documents/vaml-master/caffe-detectors/include/caffedetectorfactory.h"
+
+#include <detector_msg/bounding_box.h>
+#include <detector_msg/detection_result.h>
+ 
+class ImageConverter
+{
+public:
+    ImageConverter() : it_(nh_)
+    {
+        // Subscrive to input video feed and publish output video feed
+        image_sub_ = it_.subscribe("/sensor/camera/front/image", 1,&ImageConverter::imageCb, this);
+        
+        image_pub_ = it_.advertise("/output_video", 1);
+        detector_factory_ptr = cz::CaffeDetectorFactory::Instance();
+        // create detector
+        detector_ptr = detector_factory_ptr->createDetector(cz::CaffeDetectorFactory::DetectorMode::MOBILENET);
+        std::string net_pt = "/home/lhd/Documents/vaml-master/models/mbssd_voc/mbssd_voc_deploy.prototxt";
+        std::string net_weights = "/home/lhd/Documents/vaml-master/models/mbssd_voc/mbssd_voc.caffemodel";
+        std::string classes_name = "/home/lhd/Documents/vaml-master/models/mbssd_voc/voc_labels.txt";
+        detector_ptr->init(net_pt, net_weights, classes_name);
+        detector_ptr->setComputeMode("cpu", 0);
+    }
+ 
+    ~ImageConverter() {}
+ 
+void imageCb(const sensor_msgs::ImageConstPtr& msg){
+        const auto t1 = std::chrono::high_resolution_clock::now();
+        cv_bridge::CvImagePtr cv_ptr;
+        try
+        {
+            cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+        }
+        catch (cv_bridge::Exception& e)
+        {
+            ROS_ERROR("cv_bridge exception: %s", e.what());
+            return;
+        }    
+        // Draw an example circle on the video stream
+        // if (cv_ptr->image.rows > 60 && cv_ptr->image.cols > 60)
+        //     cv::circle(cv_ptr->image, cv::Point(50, 50), 10, CV_RGB(255,0,0));
+
+        cv::Mat img = cv_ptr->image;
+
+	    std::cout<<"read image victory"<<std::endl;
+        
+        std::vector<cz::Detection> dets;
+        detector_ptr->detect(img, dets, 0.3);
+        detector_msg::detection_result result;
+        result.header = msg->header;
+        // result.header.stamp = ros::Time::now();
+        // result.header.frame_id = "map";
+
+        detector_msg::bounding_box target;
+        for(int i = 0; i < dets.size(); ++i){
+            cv::Rect rc = dets[i].getRect();
+            target.score = dets[i].getScore();
+            target.l_x = rc.x;
+            target.l_y = rc.y;
+            target.r_x = rc.x + rc.width;
+            target.r_y = rc.x + rc.height;
+            result.boxs.push_back(target);
+        }
+        pub.publish(result);
+        const auto t2 = std::chrono::high_resolution_clock::now();
+        std::cout << "Used time:" << (t2-t1).count()*1e-6 << "ms" << std::endl;
+        detector_ptr->drawBox(img, dets);
+        cv::imshow("img", img);
+        cv::waitKey(3);    
+        // Output modified video stream
+        image_pub_.publish(cv_bridge::CvImage(std_msgs::Header(), "bgr8", img).toImageMsg());
+    }
+private:
+    ros::NodeHandle nh_;
+    ros::Publisher pub = nh_.advertise<detector_msg::detection_result>("/detection_result", 1);
+    image_transport::ImageTransport it_;
+    image_transport::Subscriber image_sub_;
+    image_transport::Publisher image_pub_;
+    cz::CaffeDetectorFactory::SharedPtr detector_factory_ptr;
+    cz::CaffeDetector::SharedPtr detector_ptr;
+    std::string net_pt;
+    std::string net_weights;
+    std::string classes_name;
+};
+ 
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "image_converter");
+  ImageConverter ic;
+  ros::spin();
+  return 0;
+}
---
+++ src/project_filter_icp/src/merge.cpp
@@ -0,0 +1,352 @@
+#include <ros/ros.h>  
+#include "std_msgs/String.h"
+#include <boost/thread.hpp>
+#include <mutex>
+#include <sstream>
+#include <boost/make_shared.hpp>
+#include <pcl/point_types.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_representation.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/filters/filter.h>
+#include <pcl/features/normal_3d.h>
+#include <pcl/registration/icp.h>
+#include <pcl/registration/icp_nl.h>
+#include <pcl/registration/transforms.h>
+#include <pcl/visualization/pcl_visualizer.h>
+#include <pcl_conversions/pcl_conversions.h>
+
+#include <Eigen/Dense>
+
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+#include <sensor_msgs/PointCloud2.h>
+
+#include <detector_msg/bounding_box.h>
+#include <detector_msg/detection_result.h>
+
+
+
+#include <message_filters/subscriber.h>
+#include <message_filters/synchronizer.h>
+#include <message_filters/sync_policies/approximate_time.h>
+
+typedef pcl::PointXYZ PointT;
+typedef pcl::PointCloud<PointT> PointCloud;
+typedef pcl::PointNormal PointNormalT;
+typedef pcl::PointCloud<PointNormalT> PointCloudWithNormals;
+
+class MyPointRepresentation : public pcl::PointRepresentation <PointNormalT>
+{
+    using pcl::PointRepresentation<PointNormalT>::nr_dimensions_;//using可以在派生类中使用基类的protected成员
+public:
+    MyPointRepresentation ()
+    {
+        nr_dimensions_ = 4;
+    }
+
+    virtual void copyToFloatArray (const PointNormalT &p, float * out) const//const修饰类的成员函数，则该成员函数不能修改类中任何非const成员函数
+    {
+        // < x, y, z, curvature >
+        out[0] = p.x;
+        out[1] = p.y;
+        out[2] = p.z;
+        out[3] = p.curvature;//这个函数是干嘛用的？
+    }
+};
+
+
+  void pairAlign (const PointCloud::Ptr cloud_src, const PointCloud::Ptr cloud_tgt, PointCloud::Ptr output, Eigen::Matrix4f &final_transform, bool downsample = false)
+  {
+
+    PointCloud::Ptr src (new PointCloud);//倒贴的
+    PointCloud::Ptr tgt (new PointCloud);//被贴的
+    pcl::VoxelGrid<PointT> grid;//下采样
+    if (downsample)
+    {
+        grid.setLeafSize (0.05, 0.05, 0.05);
+        grid.setInputCloud (cloud_src);
+        grid.filter (*src);
+        grid.setInputCloud (cloud_tgt);
+        grid.filter (*tgt);
+    }
+    else
+    {
+        src = cloud_src;
+        tgt = cloud_tgt;
+    }
+
+    PointCloudWithNormals::Ptr points_with_normals_src (new PointCloudWithNormals);//倒贴的变成带法线的
+    PointCloudWithNormals::Ptr points_with_normals_tgt (new PointCloudWithNormals);
+    pcl::NormalEstimation<PointT, PointNormalT> norm_est;//法线估算对象
+    pcl::search::KdTree<pcl::PointXYZ>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZ> ());//kdtree对象，Kd_tree是一种数据结构便于管理点云以及搜索点云，法线估计对象会使用这种结构来找到最近邻点
+    norm_est.setSearchMethod (tree);//使用kdtree的方法搜索估计法线
+    norm_est.setKSearch (30);//设置最近邻的数量，或者用setRadiusSearch，对每个点设置一个半径，用来确定法线和曲率
+    norm_est.setInputCloud (src);
+    norm_est.compute (*points_with_normals_src);//计算得到带每个点的法向量
+    pcl::copyPointCloud (*src, *points_with_normals_src);//将点云复制过去，最后得到的点云，每个点都包含坐标和法向量
+    norm_est.setInputCloud (tgt);//对被倒贴的也这样操作,弄成带法线的
+    norm_est.compute (*points_with_normals_tgt);
+    pcl::copyPointCloud (*tgt, *points_with_normals_tgt);
+
+
+    MyPointRepresentation point_representation;
+
+    float alpha[4] = {1.0, 1.0, 1.0, 1.0};//这个是干嘛的？加权曲率维度，以和坐标xyz保持平衡，没看懂
+    point_representation.setRescaleValues (alpha);
+
+
+    pcl::IterativeClosestPointNonLinear<PointNormalT, PointNormalT> reg;//创建非线性ICP对象
+    reg.setTransformationEpsilon (1e-6);//这个值一般设为1e-6或者更小。意义是什么？
+    reg.setMaxCorrespondenceDistance (1.0);//设置对应点之间的最大距离（0.1m）,在配准过程中，忽略大于该阈值的点
+    reg.setMaximumIterations (200);//迭代次数，几十上百都可能出现
+
+    reg.setPointRepresentation (boost::make_shared<const MyPointRepresentation> (point_representation));//括号内类比于：int a？？？ make_shared<T>:C创建一个shared_ptr共享指针
+    reg.setInputSource (points_with_normals_src);//非线性icp主角：带法线的点云
+    reg.setInputTarget (points_with_normals_tgt);
+
+    Eigen::Matrix4f Ti = Eigen::Matrix4f::Identity (), prev, targetToSource;//声明3个变量，其中Ti=Eigen::Matrix4f::Identity ()创建一个单位矩阵
+    PointCloudWithNormals::Ptr reg_result = points_with_normals_src;//存放icp结果
+
+    for (int i = 0; i < 30; ++i)//这个for循环是和距离阈值有关的，不断的减小距离阈值30次，相当于不断地离目标点云越来越近
+    {
+       // PCL_INFO ("Iteration Nr. %d.\n", i);
+
+        points_with_normals_src = reg_result;//将上次的拼接结果作为本次拼接的倒贴者。因为在for循环里，不是每次都定义，所以直接用reg_result这个变量
+
+        reg.setInputSource (points_with_normals_src);
+        reg.align (*reg_result);//拼接
+
+        Ti = reg.getFinalTransformation()*Ti;//将本次最终的转换矩阵累积到之前的转换矩阵
+
+        if (fabs ((reg.getLastIncrementalTransformation () - prev).sum ()) < reg.getTransformationEpsilon ())//如果上次和本次的转换矩阵差值的元素和小于我们设置的值（就是相比上一次动不了多少了）
+            reg.setMaxCorrespondenceDistance (reg.getMaxCorrespondenceDistance () - 0.01);//就将距离阈值设的小一些，把一些远的点去掉，再继续（for循环中）匹配
+        prev = reg.getLastIncrementalTransformation ();//更新上一次的变换
+
+
+    }
+
+    targetToSource = Ti.inverse();//使倒贴者和被倒贴者互换的矩阵
+
+    pcl::transformPointCloud (*cloud_tgt, *output, targetToSource);//被倒贴变倒贴了
+
+    *output += *cloud_src;//贴过去以后和人加一块，变成一家人。拼接完成。
+    final_transform = targetToSource;
+  }
+
+
+
+
+
+
+
+
+
+
+
+class SubscribeAndPublish  
+{  
+public:  
+    SubscribeAndPublish()  
+    {  
+        count = 0;
+
+        //Topic you want to publish  
+        pthread_rwlock_init(&right_lock, NULL);   //初始化读写锁
+        pthread_rwlock_init(&left_lock, NULL);   //初始化读写锁
+
+        _detection_sub = new message_filters::Subscriber<detector_msg::detection_result>(nh, "/detection_result", 1);
+        _left_cloud_sub = new message_filters::Subscriber<sensor_msgs::PointCloud2>(nh, "/sensor/lidar16/left/pointcloud", 1);
+        _right_cloud_sub = new message_filters::Subscriber<sensor_msgs::PointCloud2>(nh, "/sensor/lidar16/right/pointcloud", 1);
+
+        _sync = new message_filters::Synchronizer<SyncPolicy>(
+        SyncPolicy(10), *_detection_sub, *_left_cloud_sub, *_right_cloud_sub);
+        _sync->registerCallback(boost::bind(&SubscribeAndPublish::combineCallback, this, _1, _2, _3));
+
+
+        Eigen::Vector3d rvec(-0.0251048, 0.064995, -0.513204);// No.6  -0.0459966, -0.128626, 0.0558593
+        Eigen::Matrix3d rot = Eigen::Matrix3d::Identity();
+        double rot_angle = rvec.norm();
+	      if (std::fabs(rot_angle) > 1e-6) {
+	      	Eigen::Vector3d axis = rvec.normalized();
+          rot = Eigen::AngleAxis<double>(rot_angle, axis);
+	      }
+          
+        Eigen::Vector3d tvec(-0.63735, -1.14282, -0.0275067);//No.6  -0.066045, -1.23302, 0.0377227
+        _pairTransform.block<3,3>(0,0) = rot;
+        _pairTransform.block<3,1>(0,3) = tvec;
+        // std::cout<<pairTransform<<std::endl;
+
+
+    }  
+
+
+    void combineCallback(const detector_msg::detection_resultConstPtr &dt_result,
+                        const sensor_msgs::PointCloud2ConstPtr &left, 
+                        const sensor_msgs::PointCloud2ConstPtr &right){ 
+        // std::cout<<dt_result->boxs.size()<<std::endl;
+        dt_result->boxs[0].l_x;
+        pcl::fromROSMsg (*right, *cloud_right);
+        // ROS_INFO("get right cloud message");
+        pcl::fromROSMsg (*left, *cloud_left);
+        // ROS_INFO("get left cloud message");
+        cloud_raw_merge->clear();
+        *cloud_raw_merge += *cloud_left;
+        pcl::PointCloud<pcl::PointXYZ>::Ptr transformed_cloud (new pcl::PointCloud<pcl::PointXYZ> ());    
+        pcl::transformPointCloud (*cloud_right, *transformed_cloud, _pairTransform);
+        *cloud_raw_merge += *transformed_cloud;
+        // std::cout<<cloud_raw_merge->size()<<std::endl;
+
+        project_pc_image(image_origin,cloud_raw_merge);
+        
+        sensor_msgs::PointCloud2 ros_cloud;
+        pcl::toROSMsg(*cloud_raw_merge, ros_cloud);
+        ros_cloud.header = left->header;
+        _pub.publish(ros_cloud);
+
+
+    }
+
+    void data_clean_remove_movable(){
+        
+    }
+
+
+    bool in_bounding_box(cv::Point2f pt,float &l_x,float &l_y,float &r_x,float &r_y){
+        if( pt.x < l_x || pt.y > l_y || pt.x > r_x || pt.y < r_y){
+            return false;
+            }
+        return true;
+    }
+    
+    void project_pc_image(cv::Mat & img,pcl::PointCloud<pcl::PointXYZ>::Ptr cloud){
+        
+        // cv::Mat image_origin = cv::imread("/media/data/temp/image/0.jpeg");
+        // pcl::PointCloud<pcl::PointXYZI>::Ptr cloud_origin(new pcl::PointCloud<pcl::PointXYZI>);
+        pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_withoutNAN(new pcl::PointCloud<pcl::PointXYZ>);
+        // pcl::io::loadPCDFile<pcl::PointXYZI> ("/media/liuzhiyang/data/temp/pcd/0.pcd", *cloud_origin);
+        std::vector<int> indices;
+        pcl::removeNaNFromPointCloud(*cloud, *cloud_withoutNAN, indices);
+
+        std::vector<cv::Point3f> pts_3d;
+        for (size_t i = 0; i < cloud->size(); ++i)
+        {
+            pcl::PointXYZ point_3d = cloud_withoutNAN->points[i];
+            // if (point_3d.x > 2  && point_3d.y > 2 && point_3d.z > -2)
+            {
+                pts_3d.emplace_back(cv::Point3f(point_3d.x, point_3d.y, point_3d.z));
+            }
+        }
+
+        // using iterator
+
+        // read calibration parameter
+
+        // cv::Mat<double>(3, 3) camera_matrix(1673.764472, 0.0, 615.269162, 0.0, 1671.726940, 486.603777 0.0, 0.0, 1.0);
+        // cv::Mat<double>(1,4) distortion_coeff(-0.093271, 0.295162, -0.002398, 0.000073); 
+        // cv::Mat<double>(3,1) r_vec(-0.63735, -1.14282, -0.0275067);
+        // cv::Mat<double>(3,1) t_vec(-0.0251048, 0.064995, -0.513204);
+
+        // project 3d-points into image view
+        std::vector<cv::Point2f> pts_2d;
+        cv::projectPoints(pts_3d, _leftlidar_to_camera_rvec, _leftlidar_to_camera_tvec, _camera_intrinsic_coeff, _camera_distortion_coeff, pts_2d);
+        cv::Mat img_show = cv::Mat::zeros(img.size(),CV_8UC3);// img.clone()
+        int image_rows = image_origin.rows;
+        int image_cols = image_origin.cols;
+        std::cout<<pts_2d.size()<<std::endl;
+        int count=0;
+        for (size_t i = 0; i < pts_2d.size(); ++i)
+        {
+            cv::Point2f point_2d = pts_2d[i];
+            if (point_2d.x < 0 || point_2d.x > image_cols || point_2d.y < 0 || point_2d.y > image_rows)
+            {
+                continue;
+            }
+            else
+            {
+                img_show.at<cv::Vec3b>(point_2d.y, point_2d.x)[0] = 255;
+                img_show.at<cv::Vec3b>(point_2d.y, point_2d.x)[1] = 255;
+                img_show.at<cv::Vec3b>(point_2d.y, point_2d.x)[2] = 255;
+                ++count;
+            }
+
+            if (point_2d.x > 0 && point_2d.x < image_cols && point_2d.y > 0 && point_2d.y < image_rows)
+            {
+                img_show.at<cv::Vec3b>(point_2d.y, point_2d.x)[0] = 255;
+                img_show.at<cv::Vec3b>(point_2d.y, point_2d.x)[1] = 255;
+                img_show.at<cv::Vec3b>(point_2d.y, point_2d.x)[2] = 255;
+                ++count;
+
+            } 
+            else
+            {
+                continue;
+            }  
+        }
+        std::cout<<"project points: "<<count<<std::endl;
+        cv::imshow("img_projection", img_show);
+        cv::waitKey(3); 
+    }
+
+
+private:  
+    ros::NodeHandle nh;   
+    ros::Publisher _pub = nh.advertise<sensor_msgs::PointCloud2>("/chatter_pub", 1);;
+    message_filters::Subscriber<detector_msg::detection_result> *_detection_sub;
+    message_filters::Subscriber<sensor_msgs::PointCloud2>  *_left_cloud_sub, *_right_cloud_sub;
+    
+    // 消息同步
+    typedef message_filters::sync_policies::ApproximateTime<detector_msg::detection_result, 
+                sensor_msgs::PointCloud2, sensor_msgs::PointCloud2> SyncPolicy;
+    message_filters::Synchronizer<SyncPolicy> *_sync;
+    
+    
+
+    std_msgs::String output;
+    pthread_rwlock_t right_lock;
+    pthread_rwlock_t left_lock;
+    int count; 
+
+
+    typedef boost::shared_lock<boost::shared_mutex> read_lock;
+    typedef boost::unique_lock<boost::shared_mutex> write_lock;
+
+
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_left {new pcl::PointCloud<pcl::PointXYZ>};//转换为pcl格式
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_right {new pcl::PointCloud<pcl::PointXYZ>};//每两个相加的结果
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_raw_merge {new pcl::PointCloud<pcl::PointXYZ>};
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_former {new pcl::PointCloud<pcl::PointXYZ>};//加工后的pcl格式
+    sensor_msgs::PointCloud2 cloud_out;//转换为ros格式
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_last{new pcl::PointCloud<pcl::PointXYZ>};
+    
+    //right lidar to left lidar
+    Eigen::Matrix4d _pairTransform = Eigen::Matrix4d::Identity (); 
+    //lidar_camera
+    cv::Mat _camera_intrinsic_coeff = (cv::Mat_<double>(3,3,CV_64F) << 1673.764472, 0.0, 615.269162, 0.0, 1671.726940, 486.603777 , 0.0, 0.0, 1.0);
+    
+
+    cv::Mat _camera_distortion_coeff = (cv::Mat_<double>(1,4,CV_64F) << -0.093271, 0.295162, -0.002398, 0.000073); 
+    cv::Mat _leftlidar_to_camera_tvec = (cv::Mat_<double>(3,1,CV_64F) << -0.7, -0.4, 0.3);
+    cv::Mat _leftlidar_to_camera_rvec = (cv::Mat_<double>(3,1,CV_64F) << 0.861723, -1.48802, 1.63929);
+
+    cv::Mat image_origin = cv::imread("/home/lhd/Documents/vaml-master/data/src.jpeg");
+    // 
+};//End of class SubscribeAndPublish  
+
+int main(int argc, char **argv)  
+{  
+  //Initiate ROS  
+  ros::init(argc, argv, "subscribe_and_publish");  
+
+  //Create an object of class SubscribeAndPublish that will take care of everything  
+  SubscribeAndPublish test;  
+  ros::spin();
+
+  return 0;  
+}  
+
