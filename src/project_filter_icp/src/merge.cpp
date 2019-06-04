f84228bb2000d73a7f6bb5e365c477fb74ab8564
ArmendLee <1003255122@qq.com>
2019-06-04 15:21:50 +0800

    5.4

--- src/project_filter_icp/src/merge.cpp
+++ src/project_filter_icp/src/merge.cpp
@@ -1,13 +1,18 @@
 #include <ros/ros.h>  
-#include "std_msgs/String.h"
+#include <message_filters/subscriber.h>
+#include <message_filters/synchronizer.h>
+#include <message_filters/sync_policies/approximate_time.h>
+
 #include <boost/thread.hpp>
+#include <boost/make_shared.hpp>
 #include <mutex>
 #include <sstream>
-#include <boost/make_shared.hpp>
+
 #include <pcl/point_types.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_representation.h>
 #include <pcl/io/pcd_io.h>
+#include <pcl/io/ply_io.h>
 
 #include <pcl/filters/radius_outlier_removal.h>
 #include <pcl/filters/voxel_grid.h>
@@ -33,70 +38,33 @@
 #include <detector_msg/bounding_box.h>
 #include <detector_msg/detection_result.h>
 
-
-
-#include <message_filters/subscriber.h>
-#include <message_filters/synchronizer.h>
-#include <message_filters/sync_policies/approximate_time.h>
+#include <ros_msgs/Odometry.h>
+#include "algorithm/feature3d/pcl_utils.h"
 
 typedef pcl::PointXYZ PointT;
 typedef pcl::PointCloud<PointT> PointCloud;
 typedef pcl::PointNormal PointNormalT;
 typedef pcl::PointCloud<PointNormalT> PointCloudWithNormals;
 
-class MyPointRepresentation : public pcl::PointRepresentation <PointNormalT>
-{
-    using pcl::PointRepresentation<PointNormalT>::nr_dimensions_;//using可以在派生类中使用基类的protected成员
-public:
-    MyPointRepresentation ()
-    {
-        nr_dimensions_ = 4;
-    }
-
-    virtual void copyToFloatArray (const PointNormalT &p, float * out) const//const修饰类的成员函数，则该成员函数不能修改类中任何非const成员函数
-    {
-        // < x, y, z, curvature >
-        out[0] = p.x;
-        out[1] = p.y;
-        out[2] = p.z;
-        out[3] = p.curvature;//这个函数是干嘛用的？
-    }
-};
-
-
-  
-
-
-
-
-
-
-
-
-
-
-
 class SubscribeAndPublish  
 {  
 public:  
     SubscribeAndPublish()  
     {  
-        count = 0;
-
-        //Topic you want to publish  
-        pthread_rwlock_init(&right_lock, NULL);   //初始化读写锁
-        pthread_rwlock_init(&left_lock, NULL);   //初始化读写锁
+        _count = 0;       
 
         _detection_sub = new message_filters::Subscriber<detector_msg::detection_result>(nh, "/detection_result", 1);
         _left_cloud_sub = new message_filters::Subscriber<sensor_msgs::PointCloud2>(nh, "/sensor/lidar16/left/pointcloud", 1);
         _right_cloud_sub = new message_filters::Subscriber<sensor_msgs::PointCloud2>(nh, "/sensor/lidar16/right/pointcloud", 1);
-
+        _pose_sub = new message_filters::Subscriber<ros_msgs::Odometry>(nh, "/tergeo/localization/pose", 1);
 
         _sync = new message_filters::Synchronizer<SyncPolicy>(
-        SyncPolicy(10), *_detection_sub, *_left_cloud_sub, *_right_cloud_sub);
-        _sync->registerCallback(boost::bind(&SubscribeAndPublish::combineCallback, this, _1, _2, _3));
+        SyncPolicy(10), *_detection_sub, *_left_cloud_sub, *_right_cloud_sub, *_pose_sub);
+        _sync->registerCallback(boost::bind(&SubscribeAndPublish::combineCallback, this, _1, _2, _3,_4)); 
+        initLidarExtrinsics();       
+    }  
 
-
+    void initLidarExtrinsics(){
         Eigen::Vector3f rvec(-0.0251048, 0.064995, -0.513204);// No.6  -0.0459966, -0.128626, 0.0558593
         Eigen::Matrix3f rot = Eigen::Matrix3f::Identity();
         double rot_angle = rvec.norm();
@@ -108,18 +76,19 @@ public:
         Eigen::Vector3f tvec(-0.63735, -1.14282, -0.0275067);//No.6  -0.066045, -1.23302, 0.0377227
         _pairTransform.block<3,3>(0,0) = rot;
         _pairTransform.block<3,1>(0,3) = tvec;
-    }  
-
-
+    }
     void combineCallback(const detector_msg::detection_resultConstPtr &dt_result,
                         const sensor_msgs::PointCloud2ConstPtr &left, 
-                        const sensor_msgs::PointCloud2ConstPtr &right){ 
+                        const sensor_msgs::PointCloud2ConstPtr &right,const ros_msgs::OdometryConstPtr &pose){ 
         dt_result->boxs[0].l_x;
+
+        update_pose(pose);
+
         pcl::fromROSMsg (*right, *cloud_right);
         pcl::fromROSMsg (*left, *cloud_left);
         cloud_raw_merge->clear();
         *cloud_raw_merge += *cloud_left;
-    
+        
 
         pcl::PointCloud<pcl::PointXYZ>::Ptr transformed_cloud (new pcl::PointCloud<pcl::PointXYZ> ());    
         pcl::transformPointCloud (*cloud_right, *transformed_cloud, _pairTransform);
@@ -130,26 +99,50 @@ public:
         const auto project_t2 = std::chrono::high_resolution_clock::now();
         ROS_INFO("Project a cloud cost %f ms",(project_t2-project_t1).count()*1e-6);
 
+        pcl::PointCloud<pcl::PointXYZ>::Ptr without_ground_cloud (new pcl::PointCloud<pcl::PointXYZ> ());
+        const auto rm_ground_t1 = std::chrono::high_resolution_clock::now();
+        remove_ground(cloud_raw_merge,without_ground_cloud);
+        const auto rm_ground_t2 = std::chrono::high_resolution_clock::now();
+        ROS_INFO("remove ground cost %f ms",(rm_ground_t2-rm_ground_t1).count()*1e-6);
+
         const auto t1 = std::chrono::high_resolution_clock::now();
         if(cloud_former->size()>0){
-            pairAlign(cloud_raw_merge, cloud_former, cloud_all, _sequence_transform, false);
+            pairAlign(without_ground_cloud, cloud_former, cloud_pair, _sequence_transform, false);
             // std::cout<<_sequence_transform<<std::endl;
         }
+
         const auto t2 = std::chrono::high_resolution_clock::now();
         ROS_INFO("Total ICP cost %f ms",(t2-t1).count()*1e-6);
 
         cloud_former->clear();
-        pcl::copyPointCloud(*cloud_raw_merge,*cloud_former);
+        pcl::copyPointCloud(*without_ground_cloud,*cloud_former);
 
 
         sensor_msgs::PointCloud2 ros_cloud;
-        pcl::toROSMsg(*cloud_all, ros_cloud);//topic pulicted
+        pcl::toROSMsg(*cloud_pair, ros_cloud);//topic pulicted
         ros_cloud.header = left->header;
         ros_cloud.header.frame_id = "camera_init";
         _pub.publish(ros_cloud);
     }
 
+    void remove_ground(const pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_src,pcl::PointCloud<pcl::PointXYZ>::Ptr& withoutGround){
+        Eigen::Matrix<double, 4, 1> plane_coeff;
+        std::vector<int> inliers;
+        tergeo::algorithm::PclUtils::extractPlane(cloud_src,0.2,plane_coeff,inliers);
+        tergeo::algorithm::PclUtils::extractIndices(cloud_src,withoutGround,inliers,true);
+    }
 
+    void update_pose(const ros_msgs::OdometryConstPtr &pose){
+        Eigen::Matrix4f tmp;
+        tmp<< pose->R[0], pose->R[1], pose->R[2], pose->x,
+                 pose->R[3], pose->R[4], pose->R[5], pose->y,
+                 pose->R[6], pose->R[7], pose->R[8], pose->z,
+                 0,0,0,1;
+        if(cloud_former->size()>0){            
+            _sequence_transform = _former_status * tmp.inverse();
+        }
+        _former_status = tmp; 
+    }
 
     bool in_bounding_box(cv::Point2f &pt,const float &l_x,const float &l_y,const float &r_x,const float &r_y){
         if( pt.x < l_x || pt.y < l_y || pt.x > r_x || pt.y > r_y){
@@ -169,7 +162,7 @@ public:
         {
             pcl::PointXYZ point_3d = cloud_withoutNAN->points[i];
             if (!(point_3d.x < 0 && point_3d.x > -3 && abs(point_3d.y) < 1.3)
-                && point_3d.x < 50 && point_3d.x > -50 && point_3d.y < 50 && point_3d.y > -50)//截取周围环境点云
+                && point_3d.x < 25 && point_3d.x > -25 && point_3d.y < 25 && point_3d.y > -25)//截取周围环境点云
             {
                 pts_3d.emplace_back(cv::Point3f(point_3d.x, point_3d.y, point_3d.z));
             }
@@ -181,7 +174,6 @@ public:
         cv::Mat img_show = cv::Mat::zeros(img.size(),CV_8UC3);
         int image_rows = image_origin.rows;
         int image_cols = image_origin.cols;
-        int count=0;
         bool outside;
         for (size_t i = 0; i < pts_2d.size(); ++i)
         {
@@ -213,7 +205,7 @@ public:
         pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_to_filter(new pcl::PointCloud<pcl::PointXYZ>),cloud_tmp(new pcl::PointCloud<pcl::PointXYZ>);
         const auto cp_t1 = std::chrono::high_resolution_clock::now();
 
-        *cloud_to_filter = *cloud_src;
+        pcl::transformPointCloud (*cloud_src, *cloud_to_filter, _sequence_transform);
         
         const auto cp_t2 = std::chrono::high_resolution_clock::now();
         ROS_INFO("Copy a cloud frame cost %f ms",(cp_t2-cp_t1).count()*1e-6);
@@ -260,6 +252,16 @@ public:
         output->clear();
         *output += *cloud_former;
         *output += *tmp;    
+        *cloud_all += *tmp;
+        _count++;
+        if(_count > 10){                        
+            std::string filename = "./cloud_saved.pcd";
+            // pcl::io::savePCDFileASCII (filename, *cloud_all); //将点云保存到PCD文件中
+            filename += ".ply";
+            pcl::io::savePLYFileBinary (filename, *cloud_all);
+            _count = 0;
+            cloud_all->clear();
+        }
     }
 
 private:  
@@ -267,23 +269,21 @@ private:
     ros::Publisher _pub = nh.advertise<sensor_msgs::PointCloud2>("/laser_cloud_surround", 1);;
     message_filters::Subscriber<detector_msg::detection_result> *_detection_sub;
     message_filters::Subscriber<sensor_msgs::PointCloud2>  *_left_cloud_sub, *_right_cloud_sub;
-    
+    message_filters::Subscriber<ros_msgs::Odometry> *_pose_sub;
+
     // 消息同步
     typedef message_filters::sync_policies::ApproximateTime<detector_msg::detection_result, 
-                sensor_msgs::PointCloud2, sensor_msgs::PointCloud2> SyncPolicy;
+                sensor_msgs::PointCloud2, sensor_msgs::PointCloud2, ros_msgs::Odometry> SyncPolicy;
     message_filters::Synchronizer<SyncPolicy> *_sync;
     
 
-    std_msgs::String output;
-    pthread_rwlock_t right_lock;
-    pthread_rwlock_t left_lock;
-    int count;
+    int _count;
 
 
     typedef boost::shared_lock<boost::shared_mutex> read_lock;
     typedef boost::unique_lock<boost::shared_mutex> write_lock;
 
-
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_pair {new pcl::PointCloud<pcl::PointXYZ>};//前后两帧匹配的cloud
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_all {new pcl::PointCloud<pcl::PointXYZ>};//所有的cloud
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_left {new pcl::PointCloud<pcl::PointXYZ>};//转换为pcl格式
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_right {new pcl::PointCloud<pcl::PointXYZ>};//每两个相加的结果
@@ -296,7 +296,7 @@ private:
     Eigen::Matrix4f _pairTransform = Eigen::Matrix4f::Identity (); //左右lidar 相对位姿
 
     Eigen::Matrix4f _sequence_transform = Eigen::Matrix4f::Identity ();
-
+    Eigen::Matrix4f _former_status = Eigen::Matrix4f::Identity();
     //lidar_camera
     cv::Mat _camera_intrinsic_coeff = (cv::Mat_<double>(3,3,CV_64F) << 1673.764472, 0.0, 615.269162, 0.0, 1671.726940, 486.603777 , 0.0, 0.0, 1.0);
     
@@ -306,6 +306,8 @@ private:
     cv::Mat _leftlidar_to_camera_rvec = (cv::Mat_<double>(3,1,CV_64F) << 0.861723, -1.48802, 1.63929);
 
     cv::Mat image_origin = cv::imread("/home/lhd/Documents/vaml-master/data/src.jpeg");
+
+    int _frame;
     // 
 };//End of class SubscribeAndPublish  
 
